18117a0c8234bb2c60ba9062b359a11e8d48f9fc
jcarmac3 <pokemonpower92@gmail.com>
2025-06-21 17:59:20 -0600

    chore: Nah man, we do push

--- docker/runners/go/run_test.sh
+++ docker/runners/go/run_test.sh
@@ -2,9 +2,8 @@
 set -e
 
 SERVICE_NAME=${1:-"api"}
-TARGET_ENV=${TARGET_ENV:-"staging"}
 
-echo "🧪 Running e2e tests for $SERVICE_NAME in $TARGET_ENV"
+echo "🧪 Running e2e tests for $SERVICE_NAME"
 
 # Run the appropriate tests
 make test-e2e
